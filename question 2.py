c1259cddba6a13caa55a40cf2d67150d2e00f2b3
Ruchama924 <ruchamaovadia@gmail.com>
2025-09-03 10:04:23 +0300

    Handle negative numbers in sum_digits function

--- question 2.py
+++ question 2.py
@@ -1,6 +1,6 @@
 def sum_digits(num):
     try:
-        s = str(int(num))
+        s = str(abs(int(num)))
         return sum(int(digit) for digit in s)
     except (ValueError, TypeError):
         return "invalid input"
