ea9442e157bacd8e23e3e1d6b97455862e45be62
bracha2225 <brachakal2225@gmail.com>
2025-09-03 10:07:53 +0300

    task 1 correct question 2

--- question 2.py
+++ question 2.py
@@ -1,10 +1,10 @@
 def sum_digits(num):
     try:
-        s = str(int(num))
+        s = str(abs(int(num)))
         return sum(int(digit) for digit in s)
     except (ValueError, TypeError):
         return "invalid input"
 
 
-num2 = input("Enter a number: ")
+num2 = input("enter number: ")
 print(sum_digits(num2))
